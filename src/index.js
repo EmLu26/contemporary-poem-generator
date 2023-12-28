de4553f6b4c07bd9da0b0db08cd48597e1811a73
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-12-28 10:22:47 +0000

    Added basic js and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "The cat sat on the mat",
+    autoStart: true,
+    delay: 1,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
